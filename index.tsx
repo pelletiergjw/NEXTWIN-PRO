55e8c05ee8ffb9715f08374036122fa00258cd1f
pelletiergjw <pelletiergjw@gmail.com>
2025-12-28 17:54:54 +0100

    feat: Use HashRouter for routing
    Replaced BrowserRouter with HashRouter in the main application entry point. This change is beneficial for deployment environments where client-side routing might encounter issues with traditional browser history manipulation. Hash-based routing provides a more robust solution in such scenarios.

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { LanguageProvider } from './contexts/LanguageContext';
@@ -14,12 +14,12 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LanguageProvider>
         <AuthProvider>
           <App />
         </AuthProvider>
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
